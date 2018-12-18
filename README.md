f9f1b4ecc86e0c7555f6dae0591dd566b00ca1e2
Moore-ConnorS <littlemoorepower@Connors-MacBook-Pro.local>
2018-12-18 14:05:31 -0700

    added return on map function in myComponent

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ See README.md in master branch.
 
 ---
 
+## No return on map in my component
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -4,14 +4,16 @@ import './MyComponent.css';
 export default class MyComponent extends Component {
   render() {
     const letters = ['a', 'b', 'c'];
-    
+
     return (
       <div className="my-component-container">
         <h2>My Component</h2>
         {letters.map((letter, i) => {
-          <div key={i}>
-            {letter}
-          </div>
+          return (
+            <div key={i}>
+              {letter}
+            </div>
+          )
         })}
       </div>
     );
